7cbe3951d177f66d9239d37bacbc15a007de6e66
Nihan Sonkusare <sonkusarenihan14@gmail.com>
2026-03-03 00:02:01 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp
+ * 
+ * A simple Java application that prints "OOPS" to the console.
+ * This is UC1 of the OOPS Banner App assignment.
+ * 
+ * @author Nihan
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * 
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
